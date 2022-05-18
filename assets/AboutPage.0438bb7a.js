eccc6586314426bc069bb47d8a0aa23139fa05a7
raphaelschmitz <Raphael.Schmitz@conclusion.nl>
2022-05-18 22:22:41 +0200

    Updates

--- assets/AboutPage.0438bb7a.js
+++
@@ -1,1 +0,0 @@
-import{n as r,t}from"./main.c3157c05.js";const e={};function a(n,c){return t("ABOUT")}var s=r(e,[["render",a]]);export{s as default};
---
+++ assets/AboutPage.19c4cfbd.js
@@ -0,0 +1,17 @@
+import{d,r as a,o as c,c as h,a as e,w as t,f as m,t as o,n as S,h as T,e as r,i as C,F as E}from"./main.1c44d1a4.js";const j={class:"AboutSection"},l=d({props:{title:null},setup(u){return(s,p)=>{const i=a("SectionHeader");return c(),h("div",j,[e(i,null,{default:t(()=>[m(o(u.title),1)]),_:1}),S(s.$slots,"default")])}}});const k=["src"],P=d({setup(u){const s=`We leave an impact on the environment and others wherever we go and this is even hard to see in a place where you are a visitor.
+
+Okay, so you know you leave behind, but how to help or offset some of this burden on the local environment? Where to find a sustainable project to offset tourist trash? Plant trees or help with a re-wilding project? How do I know if this is even sustainable?
+
+Enter jEarny. Where you can Earn the Journey. (Or at least offset some of the cost).
+
+Our platform provides a wide array of sustainable activities at your desired destination, all with the help of a beautiful map search function (using optical satellite imagery)! Sign-up and make an impact! Follow projects and see how the impact grows overtime.`,p=`Sustainability is a complex descriptor and comprises different aspects! There is Environmental, Economic, or Society Sustainability, and these are often overlapping. The UN 2030 Agenda for Sustainable Development have broken these facets of sustainability into 17 different goals. You can further read about what each goal includes here: https://ec.europa.eu/international-partnerships/sustainable-development-goals_en.
+
+As we continue to develop as a company, and add more projects overtime and see their impact, we can better improve this sustainability description of similar items on the platform. So, make sure you rate your activity based on the guided sustainability questions after your program!`,i=[{name:"Sanne Noordegraaf",image:"/assets/img/team/sanne.jpg",description:`- Business Developer
+- Client Portfolio Manager
+- Government Outreach`},{name:"Amie Corbin",image:"/assets/img/team/amie.jpg",description:`- Earth Observation PhD
+- Environmental Modelling
+- Copywriter`},{name:"Tobias Van De Ven",image:"/assets/img/team/tobi.jpg",description:`- Business Planning
+- Corporate Law
+- Environmental & Technology Law`},{name:"Raphael Schmitz",image:"/assets/img/team/raphael.jpg",description:`- Software Development
+- Graphic Design
+- IT`}];return(x,A)=>{const _=a("DocumentSetter"),f=a("CardTitle"),g=a("CardText"),v=a("Elevation"),b=a("Column"),y=a("Row"),w=a("PageFrame");return c(),T(w,null,{default:t(()=>[e(_,{title:"jEarny | About"}),e(l,{title:"About jEarny"},{default:t(()=>[r("p",{class:"AboutParagraph"},o(s))]),_:1}),e(l,{title:"What makes projects Sustainable?"},{default:t(()=>[r("p",{class:"AboutParagraph"},o(p))]),_:1}),e(l,{title:"The Team"},{default:t(()=>[e(y,null,{default:t(()=>[(c(),h(E,null,C(i,n=>e(b,{key:n.name,columns:12,"columns-small":6},{default:t(()=>[e(v,{class:"TeamMemberCard"},{default:t(()=>[r("img",{src:n.image,class:"TeamMember__Image"},null,8,k),e(f,null,{default:t(()=>[m(o(n.name),1)]),_:2},1024),e(g,{class:"TeamMemberCard__Description"},{default:t(()=>[m(o(n.description),1)]),_:2},1024)]),_:2},1024)]),_:2},1024)),64))]),_:1})]),_:1})]),_:1})}}});export{P as default};
